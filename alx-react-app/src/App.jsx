3be38d55b719f0e2c8bef2d7fc33793dd9325124
Anetekhai Anointing <anetsanita7@gmail.com>
2025-02-15 06:08:17 +0100

    changes made to file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -13,9 +13,9 @@ function App() {
   return (
     <>
      <WelcomeMessage /> 
-     <Header/>
-     <Footer/>
-     <MainContent/>
+     <Header />
+     <MainContent />
+     <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
