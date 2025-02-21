f74c39d0ffe303081b3bfb5edb9fde00aa2d19c7
Anetekhai Anointing <anetsanita7@gmail.com>
2025-02-21 20:04:33 +0100

    update repo

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,14 @@
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
+
+function App() {
+  const userData = { name: "Jane Doe", email: "anetsanita7@gmal.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
