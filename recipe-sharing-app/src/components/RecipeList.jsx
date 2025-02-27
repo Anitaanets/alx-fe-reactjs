7d53de6b505341ed3a350f650f5508ee953f83b1
Anetekhai Anointing <anetsanita7@gmail.com>
2025-02-27 21:38:14 +0100

    update directory

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import useRecipeStore from './recipeStore';
 
 const RecipeList = () => {
@@ -6,11 +7,12 @@ const RecipeList = () => {
   return (
     <div>
       <h2>Recipe List</h2>
-      {recipes.length === 0 ? <p>No recipes added yet.</p> : null}
+      {recipes.length === 0 ? <p>No recipes yet.</p> : null}
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
