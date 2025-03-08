6e0aa928cf44182b65bbed3f023c5df9ab5924ee
Anetekhai Anointing <anetsanita7@gmail.com>
2025-03-08 22:03:58 +0100

    update repo

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,12 @@
 import RegistrationForm from './components/RegistrationForm';
+import formikForm from './components/FormikForm';
 
 function App() {
     return (
         <div>
             <h1>User Registration</h1>
             <RegistrationForm />
+            <formikForm/>
         </div>
     );
 }
