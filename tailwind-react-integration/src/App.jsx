43258583645fe79930d478c74f829b28f7d4b0f7
Anetekhai Anointing <anetsanita7@gmail.com>
2025-03-14 17:16:57 +0100

    update repo

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,10 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
     <div className="bg-blue-500 text-white p-4 text-center">
       Tailwind is working!
+      <UserProfile/>
     </div>
   );
 }
