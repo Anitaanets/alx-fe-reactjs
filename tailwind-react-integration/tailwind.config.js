2d8725f700da4f5968fb9e9da044ca743272499d
Anetekhai Anointing <anetsanita7@gmail.com>
2025-03-14 16:03:24 +0100

    make updates

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
+  purge: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: 'class', // or 'media' for automatic dark mode
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
