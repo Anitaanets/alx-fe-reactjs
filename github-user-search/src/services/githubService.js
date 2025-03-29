204640ae62b1461a41c28f537e9d9e5982238eb5
Anetekhai Anointing <anetsanita7@gmail.com>
2025-03-29 06:04:50 +0100

    update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,24 +1,18 @@
-import axios from 'axios';
+export const fetchUserData = async (username, location = '', minRepos = '') => {
+  const query = [
+    username && `user:${username}`,
+    location && `location:${location}`,
+    minRepos && `repos:>${minRepos}`
+  ]
+    .filter(Boolean)
+    .join('+');
 
-const BASE_URL = 'https://api.github.com';
+  const response = await fetch(`https://api.github.com/search/users?q=${query}`);
 
-// Fetch user data by username
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
-    return response.data;
-  } catch (error) {
+  if (!response.ok) {
     throw new Error('User not found');
   }
-};
 
-// Advanced search for users with filters (location & repo count)
-export const fetchAdvancedSearchResults = async (username, location, minRepos) => {
-  try {
-    const query = `q=${username}+location:${location}+repos:>${minRepos}`;
-    const response = await axios.get(`${BASE_URL}/search/users?${query}`);
-    return response.data.items; // GitHub Search API returns results in `.items`
-  } catch (error) {
-    throw new Error('Error fetching advanced search results');
-  }
+  const data = await response.json();
+  return data.items; // Return array of users
 };
